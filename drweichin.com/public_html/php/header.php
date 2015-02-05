c46e69e1b01d3e7363088f83b12088b093abef78
Kyle <errorcodefive@gmail.com>
2015-02-05 11:10:56 -0500

    php updates

--- drweichin.com/public_html/php/header.php
+++ drweichin.com/public_html/php/header.php
@@ -1,29 +1,28 @@
-<?php 
-print "<header>	
-<table id=\"headTable\" width =\"100%\">
-	<tr>
-		<td id=\"logoCell\" width = \"1%\">
-			<a href = \"public_html/htmltest.html\">
-				<img id = \"logoSVG\" 
-				src=\"public_html/images/logo.svg\" 
-				alt= \"Dr. Wei Chin\" 
-				height =\"100\" 
-				width = \"100\" 
-				border= \"1\"
-				align=\"left\"
-				>
-			</a>
-		</td>
-		<td class=\"headerButton\">
-			<a href =\"testprocedures.html\" class = \"headLink\">Procedures</a>
-		</td>
-		<td class=\"headerButton\">FAQ23454</td>
-		<td class=\"headerButton\">
-			<a href = \"contact.html\" class = \"headLink\">Contact Us</a>
-		</td>
+<header>	
+<table id="headTable" width ="100%">
+		<tr>
+			<td id="logoCell" width = "1%">
+				<a href = "htmltest.html">
+					<img id = "logoSVG" 
+					src="images/logo.svg" 
+					alt= "Dr. Wei Chin" 
+					height ="100" 
+					width = "100" 
+					border= "1"
+					align="left"
+					>
+				</a>
+			</td>
+			<td class="headerButton">
+				<a href ="testprocedures.html" class = "headLink">Procedures</a>
+			</td>
+			<td class="headerButton">
+				<a href = "faq.html" class = "headLink">FAQ</a></td>
+			<td class="headerButton">
+				<a href = "contact.html" class = "headLink">Contact Us</a>
+			</td>
 
-		<td width = \"20%\"></td>
-	</tr>
-</table>
-</header>";
-?>
+			<td width = "20%"></td>
+		</tr>
+	</table>
+</header>
