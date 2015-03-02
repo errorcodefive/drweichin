14a233ae0a2dfe1cf270a429b2e6ede6e3f51cda
Kyle <errorcodefive@gmail.com>
2015-03-02 18:57:41 -0500

    created mobile css

--- drweichin.com/public_html/php/footer.php
+++ drweichin.com/public_html/php/footer.php
@@ -1,8 +1,19 @@
-<table id = "footer">
+
+<div id = "footer">
+<br>
+<table class = "footerTable">
 	<tr class = "footerButton">
-		<td><a href = "index.html">Home</a></td>
-		<td><a href = "testprocedures.html">Procedures</a></td>
-		<td><a href = "faq.html">FAQ</a></td>
-		<td><a href = "contact.html">Contact</a></td>
+		<td><a href link = "index.html">Home</a></td>
+		<td><a href link = "testprocedures.html">Procedures</a></td>
+		<td><a href link = "faq.html">FAQ</a></td>
+		<td><a href link = "contact.html">Contact</a></td>
+	</tr>
+</table>	
+<br>
+<table class = "footerTable">
+	<tr>
+		<td>Copyright Dr. Wei Chin © <?php echo Date(Y);?> </td>
 	</tr>
 </table>
+<br>
+</div>
--- drweichin.com/public_html/php/linkandmeta.php
+++ drweichin.com/public_html/php/linkandmeta.php
@@ -1,2 +1,4 @@
-		<link rel= "stylesheet" type = "text/css" href="css/htmltest.css">
+		<link rel= "stylesheet" type = "text/css" media="only screen and (max-device-width:1280px)" href="css/htmltest300px.css">
+		<link rel= "stylesheet" type = "text/css" media = "only screen and (min-device-width:1280px)" href="css/htmltest.css">
+
 		<meta name="viewport" content = "width=device-width, initial-scale=1" charset="utf-8">
