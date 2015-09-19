ff12526621c16d706dfd12f0d527a53e676e4654
Kyle <errorcodefive@gmail.com>
2015-09-19 11:48:09 -0400

    small changes to css orde so other.css takes priority

--- drweichin.com/public_html/php/materialdesignslightcn.php
+++ drweichin.com/public_html/php/materialdesignslightcn.php
@@ -1,7 +1,7 @@
+	<link rel="stylesheet" type = "text/css" href = "../css/other.css">
 	<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.indigo-pink.min.css">
 	<script src="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.min.js"></script>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-	<link rel="stylesheet" type = "text/css" href = "../css/other.css">
 	<link rel="stylesheet" type = "text/css" href = "../css/simple-slideshow-styles.css">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
